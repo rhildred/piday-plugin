0425a686f518210195b824f71fa917e8ef85ae96
Richard Hildred <rhildred@gmail.com>
2018-01-04 19:02:41 -0500

    added injection of attributes, anonymous functions

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 /*
 Plugin Name: Today's Weather
 */
-function weather_shortcodes_init()
-{
-    function weather_shortcode($atts = [], $content = null)
-    {
-        
+
+add_action('init', function(){
+    add_shortcode('weather-plugin', function($atts = [], $content = null){
+        $content .= "<script>var atts = " .
+            json_encode($atts) .
+            "</script>";
         $content .= file_get_contents(dirname(__FILE__)."/weather.html");
         return $content;
-    }
-    add_shortcode('weather-plugin', 'weather_shortcode');
-}
-add_action('init', 'weather_shortcodes_init');
+    });
+    
+});
